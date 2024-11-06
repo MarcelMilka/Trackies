db89edcf7fe579af3b92f285231e4e8dee0d30f3
Marcel Milka <m.milka.1996@wp.pl>
2024-11-06 21:54:52 +0100

    - Tested TrackiesDAO.kt.
    - Described functionalities of every method in TrackiesDAO.kt.

--- app/src/main/java/com/example/trackies/isSignedIn/user/dao/TrackiesDAO.kt
+++ app/src/main/java/com/example/trackies/isSignedIn/user/dao/TrackiesDAO.kt
@@ -3,12 +3,15 @@ package com.example.trackies.isSignedIn.user.dao
 import androidx.room.Dao
 import androidx.room.Delete
 import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import com.example.trackies.isSignedIn.user.buisness.entities.Trackie
 
 @Dao
 interface TrackiesDAO {
 
+//  This method fetches all rows from the table "Trackies" which have value true
+//  assigned to a particular column.
     @Query( """
     SELECT * FROM Trackies WHERE
     CASE :currentDayOfWeek
@@ -23,9 +26,21 @@ interface TrackiesDAO {
     """)
     suspend fun getTrackiesForToday(currentDayOfWeek: String): List<Trackie>?
 
-    @Insert
+
+//  This method is simply responsible for adding new trackie.
+//  Although UI does not allow to add a trackie named after one already existing,
+//  OnConflictStrategy was added for the sake something goes wrong.
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
     suspend fun addNewTrackie(trackie: Trackie)
 
+
+//  This method finds a row with name equal to the value of the parameter, deleting a Trackie
+//  from the database "Trackies".
     @Query("DELETE FROM Trackies WHERE name = :nameOfTrackie")
     suspend fun deleteTrackie(nameOfTrackie: String)
+
+
+//  This method gets all rows from the column "Trackies".
+    @Query("SELECT * FROM Trackies")
+    suspend fun getAllTrackies(): List<Trackie>
 }
